cac4d8e5d367a2a94b607b563b52c9a8f07b9485
alkim media - Kerim Alkim <info@alkim.de>
2021-05-06 13:37:50 +0200

    center position of the amazon login button

--- new_files/includes/extra/application_bottom/amazon_pay.php
+++ new_files/includes/extra/application_bottom/amazon_pay.php
@@ -2,4 +2,4 @@
 $checkoutHelper = new \AlkimAmazonPay\CheckoutHelper();
 echo $checkoutHelper->getJs();
 ?>
-<style>.amazon-pay-button,.amazon-login-button{clear:both;}</style>
+<style>.amazon-pay-button,.amazon-login-button{clear:both; border-top: 10px solid transparent;}.amazon-login-button{margin-left:auto;margin-right:auto;}</style>
